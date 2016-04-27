ec743b2ef5ca393d25007673264cf33dd3c66ad3
jgavirias13 <jgavirias13@gmail.com>
2016-04-26 22:58:53 -0500

    Se modifica nombre de constantes a mayusculas, cambios en comentarios e identación de codigo

--- Practica2/Practica2/Pratica2.cpp
+++ Practica2/Practica2/Pratica2.cpp
@@ -1,6 +1,6 @@
 #include "iostream"
 #include "Practica2.h"
-#define mSize 4
+#define MSIZE 4
 
 using namespace std;
 
@@ -16,91 +16,91 @@ int main()
 		input = mostrarMenu(); // Muestra las opciones
 		operacion = 0; // Limpia la operacion anterior si es que existe
 		switch (input){ // Escoge la opcion que recibio en MostrarMenu();
-		case 1:
-			// Ingresar matriz dato a dato
-			cout << endl << "Escoja la matriz a llenar:" << endl;
-			cout << "1) Matriz #1" << endl;
-			cout << "2) Matriz #2" << endl;
-			cout << "3) Volver al menu." << endl << endl;
-			cout << "Opcion:";
-			cin >> input;
-			if (input == 1) {
-				cout << "Ingrese los valores para la matriz #1" << endl;
-				matriz1 = LlenarMatriz(matriz1);
-			}
-			else if(input == 2){
-				cout << "Ingrese los valores para la matriz #2" << endl;
-				matriz2 = LlenarMatriz(matriz2);
-			}
-			break;
-		case 2:
-			//Ingresar toda la matriz
-			cout << "Operacion aun no soportada." << endl;
-			system("pause");
-			cout << endl;
-			break;
-		case 3:
-			//Ingresar toda la matriz
-			cout << endl << "Escoja la matriz a imprimir:" << endl;
-			cout << "1) Matriz #1" << endl;
-			cout << "2) Matriz #2" << endl;
-			cout << "3) Matriz Resultado" << endl;
-			cout << "4) Volver al menu." << endl << endl;
-			cout << "Opcion:";
-			cin >> input;
-			if (input == 1) {
-				imprimirMatriz(matriz1);
-			}
-			else if (input == 2) {
-				imprimirMatriz(matriz2);
-			}
-			else if(input == 3){
-				imprimirMatriz(matrizResultado);
-			}
-			break;
-		case 4:
-			//Mostrar operaciones
-			operacion = mostrarOperaciones();
-			break;
-		case 5:
-			//Termina el programa y borra las matrices
-			for (int i = 0; i < mSize; i++) {
-				delete[] matriz1[i];
-				delete[] matriz2[i];
-				delete[] matrizResultado[i];
-			}
-			delete[] matriz1;
-			delete[] matriz2;
-			delete[] matrizResultado;
-			exit(1);
-		default:
-			//Excepción
-			cout << "Entrada invalida." << endl;
-			break;
+			case 1:
+				// Ingresar matriz dato a dato
+				cout << endl << "Escoja la matriz a llenar:" << endl;
+				cout << "1) Matriz #1" << endl;
+				cout << "2) Matriz #2" << endl;
+				cout << "3) Volver al menu." << endl << endl;
+				cout << "Opcion:";
+				cin >> input;
+				if (input == 1) {
+					cout << "Ingrese los valores para la matriz #1" << endl;
+					matriz1 = LlenarMatriz(matriz1);
+				}
+				else if(input == 2){
+					cout << "Ingrese los valores para la matriz #2" << endl;
+					matriz2 = LlenarMatriz(matriz2);
+				}
+				break;
+			case 2:
+				//Ingresar toda la matriz por archivo
+				cout << "Operacion aun no soportada." << endl;
+				system("pause");
+				cout << endl;
+				break;
+			case 3:
+				//Imprimir toda la matriz
+				cout << endl << "Escoja la matriz a imprimir:" << endl;
+				cout << "1) Matriz #1" << endl;
+				cout << "2) Matriz #2" << endl;
+				cout << "3) Matriz Resultado" << endl;
+				cout << "4) Volver al menu." << endl << endl;
+				cout << "Opcion:";
+				cin >> input;
+				if (input == 1) {
+					imprimirMatriz(matriz1);
+				}
+				else if (input == 2) {
+					imprimirMatriz(matriz2);
+				}
+				else if(input == 3){
+					imprimirMatriz(matrizResultado);
+				}
+				break;
+			case 4:
+				//Mostrar operaciones
+				operacion = mostrarOperaciones();
+				break;
+			case 5:
+				//Termina el programa y borra las matrices
+				for (int i = 0; i < MSIZE; i++) {
+					delete[] matriz1[i];
+					delete[] matriz2[i];
+					delete[] matrizResultado[i];
+				}
+				delete[] matriz1;
+				delete[] matriz2;
+				delete[] matrizResultado;
+				exit(1);
+			default:
+				//Excepción
+				cout << "Entrada invalida." << endl;
+				break;
 		}
 
 		if (operacion != 0) {
 			// Esto no es así porque hay que iterar en Assembler.
 			expMatrices(matriz1,matriz2); //Carga las matrices en posiciones accesibles desde ASM
 			switch (input){
-			case 0:
-				//Suma en ASM
-				matrizResultado[0][0] = pSuma(matriz1[0][0], matriz2[0][0]);
-				break;
-			case 1:
-				//Resta en ASM
-				matrizResultado[0][0] = pResta(matriz1[0][0], matriz2[0][0]);
-				break;
-			case 2:
-				//Multiplicacion en ASM
-				matrizResultado[0][0] = pMultiplicacion(matriz1[0][0], matriz2[0][0]);
-				break;
-			case 3:
-				//Division en ASM
-				matrizResultado[0][0] = pDivision(matriz1[0][0], matriz2[0][0]);
-				break;
-			default:
-				break;
+				case 0:
+					//Suma en ASM
+					matrizResultado[0][0] = pSuma(matriz1[0][0], matriz2[0][0]);
+					break;
+				case 1:
+					//Resta en ASM
+					matrizResultado[0][0] = pResta(matriz1[0][0], matriz2[0][0]);
+					break;
+				case 2:
+					//Multiplicacion en ASM
+					matrizResultado[0][0] = pMultiplicacion(matriz1[0][0], matriz2[0][0]);
+					break;
+				case 3:
+					//Division en ASM
+					matrizResultado[0][0] = pDivision(matriz1[0][0], matriz2[0][0]);
+					break;
+				default:
+					break;
 			}
 		}
 	}
@@ -111,7 +111,7 @@ int mostrarMenu() {
 	cout << endl << "Menu principal:" << endl;
 	cout << "1) Para ingresar una matriz elemento por elemento." << endl;
 	cout << "2) Ingresar una matriz por archivo." << endl;
-	cout << "3) Imprmir una matriz." << endl;
+	cout << "3) Imprimir una matriz." << endl;
 	cout << "4) Realizar una operacion sobre las matrices." << endl;
 	cout << "5) Cerrar el programa." << endl << endl;
 	cout << "Opcion:";
@@ -134,9 +134,9 @@ int mostrarOperaciones() {
 }
 
 void imprimirMatriz(float** matriz) {
-	for (int i = 0; i < mSize; ++i){
+	for (int i = 0; i < MSIZE; ++i){
 		cout << endl;
-		for (int j = 0; j < mSize; ++j){
+		for (int j = 0; j < MSIZE; ++j){
 			cout << matriz[i][j] << "\t";
 		}
 	}
@@ -145,13 +145,13 @@ void imprimirMatriz(float** matriz) {
 
 float** AsignarMatriz() {
 	float** matriz;
-	matriz = new float*[mSize];
-	for (int i = 0; i < mSize; ++i)
+	matriz = new float*[MSIZE];
+	for (int i = 0; i < MSIZE; ++i)
 	{
-		matriz[i] = new float[mSize];
+		matriz[i] = new float[MSIZE];
 	}
-	for (int i = 0; i < mSize; ++i) {
-		for (int j = 0; j < mSize; ++j) {
+	for (int i = 0; i < MSIZE; ++i) {
+		for (int j = 0; j < MSIZE; ++j) {
 			matriz[i][j] = 0.0;
 		}
 	}
@@ -159,8 +159,8 @@ float** AsignarMatriz() {
 }
 
 float** LlenarMatriz(float** matriz) {
-	for (int i = 0; i < mSize; ++i) {
-		for (int j = 0; j < mSize; ++j) {
+	for (int i = 0; i < MSIZE; ++i) {
+		for (int j = 0; j < MSIZE; ++j) {
 			cout << "[" << i << "][" << j << "]: ";
 			cin >> matriz[i][j];
 		}
